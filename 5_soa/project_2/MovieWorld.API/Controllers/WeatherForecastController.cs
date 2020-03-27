932afb75120e3738df41ac1ae703ed8698d5be3d
fred belotte <fredbelotte@outlook.com>
2020-03-27 11:03:54 -0500

    add web api project

---
+++ 5_soa/project_2/MovieWorld.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MovieWorld.API.Controllers
+{
+  [ApiController]
+  [Route("[controller]")] //route template, http://localhost:5001/api/controller
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Blah()
+    {
+      var rng = new Random();
+      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = Summaries[rng.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+
+    [HttpGet]
+    public IEnumerable<string> Post()
+    {
+      return Summaries.ToArray();
+    }
+  }
+}
