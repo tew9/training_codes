7f8583c139007934bf0f84dd0b74a6ba89d21718
fred belotte <fredbelotte@outlook.com>
2020-03-27 14:51:23 -0500

    add console, mvc, node clients

---
+++ 5_soa/project_2_client/Webflix.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Webflix.Client
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      HttpClient client = new HttpClient();
+
+      var res = client.GetAsync("http://localhost:5000/movie").GetAwaiter().GetResult();
+
+      System.Console.WriteLine(res);
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ 5_soa/project_2_client/Webflix.MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Webflix.MVC.Models;
+
+namespace Webflix.MVC.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public string Index()
+    {
+      var client = new HttpClient();
+      return client.GetAsync("http://localhost:5000/movie").GetAwaiter().GetResult().ToString();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
